891ce9c4768675c7e3c471d307b69f7b10a6251c
Guilherme Francisco de Freitas <guifreitas89@hotmail.com>
2021-10-18 07:56:23 -0300

    Adds PRU startup to startup script

--- Autostart/simar_startup.sh
+++ Autostart/simar_startup.sh
@@ -16,6 +16,32 @@ config-pin P9_28 pruin
 config-pin P8_15 pruin
 config-pin P9_30 pruin
 
+cp ./bin/pru*.out /lib/firmware/
+
+echo pru0.out > /sys/class/remoteproc/remoteproc1/firmware
+echo pru1.out > /sys/class/remoteproc/remoteproc2/firmware
+
+echo start > /sys/class/remoteproc/remoteproc1/state
+echo start > /sys/class/remoteproc/remoteproc2/state
+
+config-pin P8_12 pruout
+config-pin P9_27 pruout
+config-pin P9_31 pruout
+config-pin P9_29 pruout
+config-pin P8_45 pruout
+config-pin P8_46 pruout
+config-pin P8_44 pruout
+config-pin P8_43 pruout
+
+config-pin P9_24 pruin
+config-pin P9_28 pruin
+config-pin P8_15 pruin
+config-pin P9_30 pruin
+config-pin P8_42 pruin
+config-pin P8_41 pruin
+config-pin P8_40 pruin
+config-pin P8_39 pruin
+
 # IO pins, just to make sure (p9_12 is reserved for w1)
 echo out > /sys/class/gpio/gpio48/direction
 echo out > /sys/class/gpio/gpio51/direction
