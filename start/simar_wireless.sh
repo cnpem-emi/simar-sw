8dd4fe033dea1de6860420cd491370c75b4f6ee5
Guilherme Francisco <guifreitas89@hotmail.com>
2021-11-24 14:57:06 -0300

    Update simar_wireless.sh

--- start/simar_wireless.sh
+++ start/simar_wireless.sh
@@ -2,7 +2,9 @@
 
 wpa_supplicant -B -P /root/simar-software/start/wpa-pid -u -s -c /etc/wpa_supplicant/wpa_supplicant.conf -i wlan0
 PID=$(cat /root/simar-software/start/wpa-pid)
+dhclient wlan0
 
+sleep 10
 while ping -c 5 10.0.38.46 >> /dev/null; do
     sleep 10
 done
