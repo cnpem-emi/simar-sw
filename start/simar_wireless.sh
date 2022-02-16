0db2e7d4687d433737f0fc980ab886c535319fa6
Guilherme Francisco <guifreitas89@hotmail.com>
2022-02-16 10:57:47 -0300

    Update simar_wireless.sh

--- start/simar_wireless.sh
+++ start/simar_wireless.sh
@@ -3,13 +3,12 @@
 SIMAR_FOLDER=/root/simar-software
 RSYNC_ADDR=10.128.114.161
 
-rsync -a --delete-after 10.128.114.161::simar $SIMAR_FOLDER --contimeout=5
-
 wpa_supplicant -B -P /root/simar-software/start/wpa-pid -u -s -c /etc/wpa_supplicant/wpa_supplicant.conf -i wlan0
 PID=$(cat /root/simar-software/start/wpa-pid)
 dhclient wlan0
 
 sleep 10
+rsync -a --delete-after 10.128.114.161::simar $SIMAR_FOLDER --contimeout=5
 while ping -c 5 10.0.38.46 >> /dev/null; do
     sleep 10
 done
