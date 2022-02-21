20ee660d4762a0355060dc83e76e61a141fc26a8
gfrn <guilherme.freitas@cnpem.br>
2022-02-21 12:37:26 -0300

    Adds CAN support to leak monitor

--- main/leak.c
+++ main/leak.c
@@ -6,6 +6,16 @@
 #include <syslog.h>
 #include <time.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
 
 #include "../spi/common.h"
 
@@ -43,7 +53,32 @@ int main(int argc, char* argv[]) {
 
   const struct timespec* period = (const struct timespec[]){{1, 0}};
 
-  while (1) {
+  int s; 
+	struct sockaddr_can addr;
+	struct ifreq ifr;
+	struct can_frame frame;
+
+	if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
+		syslog(LOG_ERR, "Could not open CAN socket");
+		return -2;
+	}
+
+	strcpy(ifr.ifr_name, "vcan0" );
+	ioctl(s, SIOCGIFINDEX, &ifr);
+
+	memset(&addr, 0, sizeof(addr));
+	addr.can_family = AF_CAN;
+	addr.can_ifindex = ifr.ifr_ifindex;
+
+	if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+		syslog(LOG_ERR, "CAN binding error");
+		return -2;
+	}
+
+	frame.can_id = 0x555;
+	frame.can_dlc = 5;
+
+  for(;;) {
     select_module(0, 2);
     spi_transfer(dummy_data, dummy_data, 1);
     select_module(0, 3);
@@ -51,6 +86,13 @@ int main(int argc, char* argv[]) {
 
     if (read(fd, digital_buffer, 1)) {
       for (int i = 0; i < 8; i++) {
+        if(digital_buffer[0] >> i & 0b00000001) {
+          snprintf(frame.data, 4, "%d %d", i, 1); // TODO: Decide what to write
+          if (write(s, &frame, sizeof(struct can_frame)) != sizeof(struct can_frame)) {
+            syslog(LOG_ERR, "CAN communication error");
+            return -2;
+          }
+        }
         reply =
             redisCommand(c, "HSET leak_detector %d %d", i, ((digital_buffer[0] >> i) & 0b00000001));
         freeReplyObject(reply);
